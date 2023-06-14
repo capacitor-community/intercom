c99aae2c3e80b2d1f9ec3b72dc583d68d4350cd1
marshall86 <antonio11061986@gmail.com>
2023-06-14 17:14:17 +0200

    Update IntercomPlugin.java
    get the status of the chat (opened or not), chat listener, get conversation count, update deprecated methods

--- android/src/main/java/com/getcapacitor/community/intercom/intercom/IntercomPlugin.java
+++ android/src/main/java/com/getcapacitor/community/intercom/intercom/IntercomPlugin.java
@@ -1,5 +1,9 @@
 package com.getcapacitor.community.intercom;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import com.getcapacitor.CapConfig;
 import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
@@ -10,7 +14,7 @@ import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.getcapacitor.annotation.Permission;
 
-import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,14 +23,41 @@ import java.util.List;
 import java.util.Map;
 
 import io.intercom.android.sdk.Intercom;
-import io.intercom.android.sdk.IntercomPushManager;
+import io.intercom.android.sdk.IntercomError;
+import io.intercom.android.sdk.IntercomStatusCallback;
+import io.intercom.android.sdk.UnreadConversationCountListener;
 import io.intercom.android.sdk.UserAttributes;
 import io.intercom.android.sdk.identity.Registration;
 import io.intercom.android.sdk.push.IntercomPushClient;
+import io.intercom.android.sdk.actions.Actions;
+import io.intercom.android.sdk.sheets.SheetListener;
 
 @CapacitorPlugin(name = "Intercom", permissions = @Permission(strings = {}, alias = "receive"))
 public class IntercomPlugin extends Plugin {
+
+    private Boolean opened = false;
+    private Integer initialUnreadMessages = 0;
     private final IntercomPushClient intercomPushClient = new IntercomPushClient();
+    private final SheetListener sheetListener = new SheetListener() {
+        @Override
+        public void onWebViewFinishedLoad() {
+            opened = true;
+            Log.i("intercom:sheetListener", String.format("onWebViewFinishedLoad - %s", opened));
+        }
+
+        @Override
+        public void onSheetTitleAction(String s) {
+        }
+
+        @Override
+        public void onCloseSheetAction() {
+            opened = false;
+            Log.i("intercom:sheetListener", String.format("onCloseSheetAction - %s", opened));
+        }
+
+        @Override
+        public void onSubmitSheetAction(Map<String, Object> map) { }
+    };
 
     @Override
     public void load() {
@@ -36,29 +67,51 @@ public class IntercomPlugin extends Plugin {
         // load parent
         super.load();
     }
-    
-    @PluginMethod()
-    public void loadWithKeys(PluginCall call) {
-        String appId = call.getString("appId", "NO_APP_ID_PASSED");
-        String apiKey = call.getString("apiKeyAndroid", "NO_API_KEY_PASSED");
-
-        Intercom.initialize(this.getActivity().getApplication(), apiKey, appId);
-     
-        // load parent
-        super.load();
-    }
 
     @Override
     public void handleOnStart() {
         super.handleOnStart();
-        bridge.getActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                //We also initialize intercom here just in case it has died. If Intercom is already set up, this won't do anything.
-                setUpIntercom();
-                Intercom.client().handlePushMessage();
-            }
+        bridge.getActivity().runOnUiThread(() -> {
+            //We also initialize intercom here just in case it has died. If Intercom is already set up, this won't do anything.
+            setUpIntercom();
+            Intercom.client().handlePushMessage();
         });
+    }
+
+    @PluginMethod
+    public void getChatOpenedStatus(PluginCall call) {
+        call.resolve(new JSObject().put("isChatOpen", opened));
+    }
+
+    @PluginMethod
+    public void getUnreadConversationCount(PluginCall call) {
+        JSObject counter = new JSObject();
+        Integer unreadMessages = Intercom.client().getUnreadConversationCount();
+        counter.put("unreadMessages", String.valueOf(unreadMessages));
+        call.resolve(counter);
+    }
+
+    @PluginMethod
+    public void addUnreadConversationCountListener(PluginCall call) {
+        Intercom.client().addUnreadConversationCountListener(unreadConversationCountListener);
+        call.resolve();
+    }
+
+    private final UnreadConversationCountListener unreadConversationCountListener = conversationCount -> handleUpdateUnreadCount();
+
+    private void handleUpdateUnreadCount() {
+        try {
+            Integer unreadMessages = Intercom.client().getUnreadConversationCount();
+
+            if(!initialUnreadMessages.equals(unreadMessages)) {
+                this.initialUnreadMessages = unreadMessages;
+               // this.opened = true;
+            }
+
+            Log.i("intercom:handleUpdateUnreadCount", String.valueOf(unreadMessages));
+        } catch (Exception e) {
+            Log.e("intercom:handleUpdateUnreadCount", "client called before Intercom initialization");
+        }
     }
 
     @PluginMethod
@@ -74,15 +127,27 @@ public class IntercomPlugin extends Plugin {
         if (userId != null && userId.length() > 0) {
             registration = registration.withUserId(userId);
         }
-        Intercom.client().registerIdentifiedUser(registration);
+        Intercom.client().loginIdentifiedUser(registration, intercomStatusCallback);
         call.resolve();
     }
 
     @PluginMethod
     public void registerUnidentifiedUser(PluginCall call) {
-        Intercom.client().registerUnidentifiedUser();
+        Intercom.client().loginUnidentifiedUser(intercomStatusCallback);
         call.resolve();
     }
+
+    private final IntercomStatusCallback intercomStatusCallback = new IntercomStatusCallback() {
+        @Override
+        public void onSuccess() {
+            Log.i("intercom:IntercomStatusCallback success", "login success");
+        }
+
+        @Override
+        public void onFailure(@NonNull IntercomError intercomError) {
+            Log.e("intercom:IntercomStatusCallback error", String.valueOf(intercomError));
+        }
+    };
 
     @PluginMethod
     public void updateUser(PluginCall call) {
@@ -109,13 +174,14 @@ public class IntercomPlugin extends Plugin {
         }
         Map<String, Object> customAttributes = mapFromJSON(call.getObject("customAttributes"));
         builder.withCustomAttributes(customAttributes);
-        Intercom.client().updateUser(builder.build());
+        Intercom.client().updateUser(builder.build(), intercomStatusCallback);
         call.resolve();
     }
 
     @PluginMethod
     public void logout(PluginCall call) {
         Intercom.client().logout();
+        Intercom.client().removeUnreadConversationCountListener(unreadConversationCountListener);
         call.resolve();
     }
 
@@ -136,6 +202,8 @@ public class IntercomPlugin extends Plugin {
     @PluginMethod
     public void displayMessenger(PluginCall call) {
         Intercom.client().displayMessenger();
+        sheetListener.onWebViewFinishedLoad();
+       // this.initialUnreadMessages = 0;
         call.resolve();
     }
 
@@ -178,7 +246,8 @@ public class IntercomPlugin extends Plugin {
 
     @PluginMethod
     public void hideInAppMessages(PluginCall call) {
-        Intercom.client().setLauncherVisibility(Intercom.GONE);
+        Log.i("intercom:hideInAppMessages", "intercom hideInAppMessages called");
+        Intercom.client().setInAppMessageVisibility(Intercom.Visibility.GONE);
         call.resolve();
     }
 
@@ -192,6 +261,7 @@ public class IntercomPlugin extends Plugin {
     @PluginMethod
     public void setUserHash(PluginCall call) {
         String hmac = call.getString("hmac");
+        assert hmac != null;
         Intercom.client().setUserHash(hmac);
         call.resolve();
     }
@@ -199,6 +269,7 @@ public class IntercomPlugin extends Plugin {
     @PluginMethod
     public void setBottomPadding(PluginCall call) {
         String stringValue = call.getString("value");
+        assert stringValue != null;
         int value = Integer.parseInt(stringValue);
         Intercom.client().setBottomPadding(value);
         call.resolve();
@@ -222,6 +293,7 @@ public class IntercomPlugin extends Plugin {
             Map message = mapFromJSON(notificationData);
             if (intercomPushClient.isIntercomPush(message)) {
                 intercomPushClient.handlePush(this.getActivity().getApplication(), message);
+                Log.i("intercomReceivePush", String.valueOf(message));
                 call.resolve();
             } else {
                 call.reject("Notification data was not a valid Intercom push message");
@@ -247,6 +319,12 @@ public class IntercomPlugin extends Plugin {
 
             // init intercom sdk
             Intercom.initialize(this.getActivity().getApplication(), apiKey, appId);
+
+            Log.i("intercom:setUpIntercom", String.format("setUpIntercom --- is chat opened? - %s", opened));
+            if(this.opened) {
+                Intercom.client().hideIntercom();
+                sheetListener.onCloseSheetAction();
+            }
         } catch (Exception e) {
             Logger.error("Intercom", "ERROR: Something went wrong when initializing Intercom. Check your configurations", e);
         }
